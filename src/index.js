bb7b6100c14e542bc49e148f3f5e4d0d83564db7
Alexandra <alexandra.tansing@gmail.com>
2020-04-14 19:56:19 -0500

    Update GA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ReactGA from 'react-ga';
 
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
@@ -9,6 +8,5 @@ import awsconfig from './aws-exports';
 import 'bootstrap/dist/css/bootstrap.css';
 
 Amplify.configure(awsconfig);
-ReactGA.initialize('UA-163494853-1');
 
 ReactDOM.render(<App />, document.getElementById('root'));
